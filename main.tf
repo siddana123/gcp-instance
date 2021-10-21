17ac6427ecefb1162adea3344a74f64c8357e299
siddu <goudasiddu@gmail.com>
2021-10-21 18:25:13 +0530

    update

--- main.tf
+++ main.tf
@@ -1,40 +1,28 @@
 provider "google" {  
   project = "unique-balancer-327214"  
   region  = "us-central1" 
-  zone    = "us-central1-c"
   }
-resource "google_compute_network" "default" { 
-     name = "terraform-network"
-}
-resource "google_compute_instance" "vm_instance" { 
-     name         = "terraform-instance" 
-     machine_type = "f1-micro"
-  boot_disk { 
-         initialize_params {     
-              image = "debian-cloud/debian-9"    
-              }
-                }
-  network_interface {    
-      network = google_compute_network.default.name 
-         access_config { 
+resource "google_compute_instance" "my_vm" {
+  name         = "tf-tutorial-vm"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
 
-            } 
-        }
-      
+  boot_disk {
 
- 
-  resource "google_compute_firewall" "allow-port-ssh" {
-  name    = "test-firewall"
- network = google_compute_network.default.name 
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "22", "8080", "1000-2000"]
+    initialize_params {
 
-  }
- allow {
-    #protocol = "icmp"
-    protocol = "ssh"
-  }
-  source_tags = ["web"]
-}
+      image = "debian-cloud/debian-10"
+
     }
+
+  }
+
+  network_interface {
+
+    network = "default"
+
+    access_config {}
+
+  }
+
+}
